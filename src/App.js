deed9520f34d4edd191fe9eacedc97219536ce5f
Anurag Tiwari <anuragtiwarigpj2005@gmail.con>
2024-03-06 18:27:35 +0530

    intermidate changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-   <h1>Hellow </h1>
+      <Router>
+        <Routes>
+          <Route path="/checkout" element={<h1>Checkout</h1>} />
+          <Route path="/login" element={<h1>Login Page</h1>} />
+          <Route path="/" element={<h1>Home Page!!!!!</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }

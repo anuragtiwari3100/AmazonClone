102f9e42e5c943b3575bdb0df495b1f2c8d96851
Anurag Tiwari <anuragtiwarigpj2005@gmail.con>
2024-03-06 19:31:15 +0530

    A few changes I made

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./Header";
+
+function Home() {
+  return <h1>Home Page!!!!!</h1>;
+}
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header /> {/* Render the Header component outside the Routes */}
         <Routes>
           <Route path="/checkout" element={<h1>Checkout</h1>} />
           <Route path="/login" element={<h1>Login Page</h1>} />
-          <Route path="/" element={<h1>Home Page!!!!!</h1>} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
@@ -16,3 +23,4 @@ function App() {
 }
 
 export default App;
+
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function Header() {
+  return (
+    <nav className='header__logo'>
+      <img src="https://pngimg.com/uploads/amazon/amazon_PNG27.png" className="img-fluid rounded-top" alt="Amazon Logo" />
+      <h1>Amazon</h1> {/* Add the text element here */}
+    </nav>
+  );
+}
+
+export default Header;
+
